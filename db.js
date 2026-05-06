6d1a5742fda9422dea05e1406edef608f959c95f
Saurabh Suman <saurabhsuman1607@gmail.com>
2026-05-06 13:37:08 +0530

    modified the env and db config files

--- db.js
+++ db.js
@@ -1,25 +1,31 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const mongoAtlasUrl = process.env.MONGO_URI;
+const mongoAtlastUrl = process.env.MONGO_URL;
+const compassUrl = process.env.DB_COMPASS_URI;
+// console.log(mongoAtlasUrl);
 
-mongoose.connect(mongoAtlasUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(mongoAtlasUrl, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+mongoose
+  .connect(mongoAtlastUrl)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("Error:", err));
 
 const db = mongoose.connection;
 
-db.on("disconnected", () => {
-  console.log("Successfully disconnected to the database");
-});
+// db.on("disconnected", () => {
+//   console.log("Successfully disconnected to the database");
+// });
 
-db.on("error", () => {
-  console.log("error in connecting to the database");
-});
+// db.on("error", () => {
+//   console.log("error in connecting to the database");
+// });
 
-db.on("connected", () => {
-  console.log("Successfully connected to the database");
-});
+// db.on("connected", () => {
+//   console.log("Successfully connected to the database");
+// });
 
 module.exports = db;
